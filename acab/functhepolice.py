61052450fb2427e7c6f16368c277172009261a3a
Fritz Francisco <fritz.a.francisco@gmail.com>
2020-11-24 16:17:19 +0100

    added linreg()

--- acab/functhepolice.py
+++ acab/functhepolice.py
@@ -2251,3 +2251,20 @@ def generate_colors(n):
     rgb_values = np.array(rgb_values)
     return rgb_values, hex_values 
 
+    
+def linreg(X, Y):
+    """
+    return a,b in solution to y = ax + b such that root mean square distance between trend line and original points is minimized.
+    plot using: extrapolatedtrendline=[a*index + b for index in range(len(ratios))]
+    """
+    
+    N = len(X)
+    Sx = Sy = Sxx = Syy = Sxy = 0.0
+    for x, y in zip(X, Y):
+        Sx = Sx + x
+        Sy = Sy + y
+        Sxx = Sxx + x*x
+        Syy = Syy + y*y
+        Sxy = Sxy + x*y
+    det = Sxx * N - Sx * Sx
+    return (Sxy * N - Sy * Sx)/det, (Sxx * Sy - Sx * Sxy)/det
