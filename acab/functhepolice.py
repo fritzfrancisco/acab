ec380cc94ab43c4c0e0da557d43d44e636abfcd6
Fritz Francisco <fritz.a.francisco@gmail.com>
2021-04-19 11:28:21 +0200

    updated get_instances() and get_visits()

--- acab/functhepolice.py
+++ acab/functhepolice.py
@@ -2390,48 +2390,50 @@ def get_incident(keys, value='2020-05-25'):
         incident = min(np.where(np.array(index) == True)[0])
     return incident
 
-def get_instances(file,
+def get_instances(input_file,
+
                   interpolate=False,
                   save=False,
                   plot=False,
-                  distance_threshold_to_roi = 0.08,
+                  distance_threshold_to_roi = None,
+                  px2m = None, #76/0.14
                   output_dir='/home/fritz/Desktop/data_20200715/'):
     '''Function to retrieve instances where xy coordinates are within a specified range.
     In this case it is specifically designed for .h5 input retrieved through track2h5()
     which contains cylinder coordinates and radii. 
     These were collected using the find_cylinder() function.'''
-    
+
     instances = {}
-    f = h5py.File(file, 'r')
+    f = h5py.File(input_file, 'r')
     keys = np.array(list(f.keys()))
-    name = file.replace('.h5', '')
+    name = input_file.replace('.h5', '')
 
     for key in keys:
         for i in np.unique(f[key][:, 3]):
             instances[str(int(i))] = {}
     f.close()
     identities = np.unique(np.array(list(instances.keys())))
-    
+
     for j, key in enumerate(keys):
         print(os.path.basename(name), np.round((j / len(keys)) * 100, 1), '%')
-        tracks = dictfromh5(file, j)
-        
+        tracks = dictfromh5(input_file, j)
+
          ## make sure trajectories for both IDs exists:
         if (len(tracks.keys()) != 2) or (np.array([tracks[str(i)]['cylinder_x'] for i in tracks]).any() == -1):
             continue
-            
+
         for i in identities:
 
             id_tracks = tracks[str(int(i))]
             id_tracks = simple_filter(id_tracks, threshold=4)
             id_tracks = rmv_out_pts(id_tracks)
-            
+
             x = id_tracks['pos_x']
             y = id_tracks['pos_y']
             cx = id_tracks['cylinder_x']
             cy = id_tracks['cylinder_y']
             cr = id_tracks['cylinder_r']
-            
+
             if interpolate == True:
                 x, _ = interpolate_signal(id_tracks['pos_x'],
                             id_tracks['frame'])
@@ -2445,12 +2447,14 @@ def get_instances(file,
             if cr.any() < 0:
                 continue
             else:
-                distances = np.sqrt((x - cx)**2 + (y - (cy))**2)
-                boolean = np.where(distances <= distance_threshold_to_roi)[0]
+                distances = np.sqrt((x - cx)**2 + (y - cy)**2)
+                boolean = np.where((distances / px2m) < distance_threshold_to_roi)[0]
+
                 instances[str(int(i))][key] = {
                     'distances': distances,
                     'boolean': boolean
                 }
+            
             if plot == True:
                 fig, ax = plt.subplots()
                 circle = plt.Circle((int(cx), int(cy)),
@@ -3031,18 +3035,21 @@ def get_time_to_roi(file,
 def consecutive(data, stepsize=1):
     return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
 
-def get_visits(file,
-               distance_threshold_to_roi = 0.08,
-               px2m = 76/0.14):
+def get_visits(input_file,
+               distance_threshold_to_roi = None,
+               px2m = None, #76/0.14
+               fps = None,
+               min_duration = 1 # minimal duration in seconds considered a visit
+              ):
     '''Function to retrieve instances where xy coordinates are within a specified range and leave this vicinity again.
     In this case it is specifically designed for .h5 input retrieved through track2h5()
-    which contains cylinder coordinates and radii. 
+    which contains cylinder coordinates and radii.
     These were collected using the find_cylinder() function.'''
 
     visits = {}
-    f = h5py.File(file, 'r')
+    f = h5py.File(input_file, 'r')
     keys = np.array(list(f.keys()))
-    name = file.replace('.h5', '')
+    name = input_file.replace('.h5', '')
 
     for key in keys:
         for i in np.unique(f[key][:, 3]):
@@ -3050,9 +3057,9 @@ def get_visits(file,
     f.close()
     identities = np.unique(np.array(list(visits.keys())))
 
-    for j, key in enumerate(keys):
+    for j, key in enumerate(keys[:4]):
         print(os.path.basename(name), np.round((j / len(keys)) * 100, 1), '%')
-        tracks = dictfromh5(file, j)
+        tracks = dictfromh5(input_file, j)
 
          ## make sure trajectories for both IDs exists:
         if (len(tracks.keys()) != 2) or (np.array([tracks[str(i)]['cylinder_x'] for i in tracks]).any() == -1):
@@ -3066,26 +3073,33 @@ def get_visits(file,
 
             x = id_tracks['pos_x']
             y = id_tracks['pos_y']
-            cy = id_tracks['cylinder_r']
             cx = id_tracks['cylinder_x']
+            cy = id_tracks['cylinder_y']
             cr = id_tracks['cylinder_r']
 
-            distances = np.sqrt((x - cx)**2 + (y - cy)**2) / px2m
-            boolean = np.where(distances <= distance_threshold_to_roi)[0]
-            in_out = consecutive(np.where(distances < distance_threshold_to_roi)[0])
 
-            if np.array(in_out,dtype=object).size != 0:
-                duration = np.nanmean(np.array([len(visit) for visit in in_out]))
-                count = len(in_out)
-            else:
-                count = 0
-                duration = 0
             if cr.any() < 0:
                 continue
             else:
+                distances = np.sqrt((x - cx)**2 + (y - cy)**2)
+                boolean = np.where((distances / px2m) < distance_threshold_to_roi)[0]            
+                in_out = consecutive(np.where((distances / px2m) < distance_threshold_to_roi)[0])
+                entry_frames = np.array([min(visit) for visit in in_out if (len(visit)/fps) >= min_duration])
+                exit_frames = np.array([max(visit) for visit in in_out if (len(visit)/fps) >= min_duration])
+                in_out = np.array([len(visit) for visit in in_out if (len(visit)/fps) >= min_duration])
+                
+                if len(in_out) > 0:
+                    duration = int(np.nanmean(in_out))
+                    count = len(in_out)
+                else:
+                    count = 0
+                    duration = 0
+
                 visits[str(int(i))][key] = {
                     'visits': count,
                     'duration': duration,
+                    'entry_frames': entry_frames,
+                    'exit_frames': exit_frames
                 }
     return visits
 
